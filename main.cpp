080101111112b0487baa4f4e2dcf5e43dd202e47
Alexander-col <aexro2221@gmail.com>
2022-03-31 15:00:43 -0400

    the task looks biusally better when compiled

--- main.cpp
+++ main.cpp
@@ -6,19 +6,19 @@
 
 int main()
 {
-  std::cout << "---------------------Example for task A---------------------" << std::endl;
-  std::cout << "Goodbye world" << std::endl;
   Coord3D pointP = {10, 20, 30};
   Coord3D pointQ = {-20, 21, -22};
+  std::cout << "Address of P: " << &pointP << std::endl;
+  std::cout << "Address of Q: " << &pointQ << std::endl;
+
+  std::cout << "---------------------Example for task A---------------------" << std::endl;
   std::cout << length(&pointP ) << std::endl;
   std::cout << length(&pointQ ) << std::endl;
-  Coord3D * ans = fartherFromOrigin(&pointP, &pointQ);
-  std::cout << ans << std::endl;
-  std::cout << "ans = " << ans << std::endl;
 
-  // std::cout << "---------------------Example for task B---------------------" << std::endl;
-  // Coord3D pointP = {10, 20, 30};
-  // Coord3D pointQ = {-20, 21, -22};
-  // Coord3D * fartherFromOrigin(&pointP, &pointQ);
+
+  std::cout << "---------------------Example for task B---------------------" << std::endl;
+  
+  Coord3D * ans = fartherFromOrigin(&pointP, &pointQ);
+  std::cout << "ans = " << ans << std::endl;
   return 0;
 }
